6426649733b87ef52b2315b9798062bcf2cc6545
Hiagor Vieira <hiagor.vieira@inovafarma.com.br>
2025-12-08 17:32:44 -0300

    improve: adds log entry.

---
+++ S-SingleResponsability/After/Loggers/ConsoleLogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SOLID_By_Example.S_SingleResponsability.After.Loggers
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message) => Console.WriteLine("[LOG] " + message);
+        public void Error(string message) => Console.WriteLine("[ERROR] " + message);
+        public void Warn(string message) => Console.WriteLine("[WARN] " + message);
+    }
+}
